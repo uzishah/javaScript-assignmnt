15b73960df59a852bf4a9179dd38ae7069d6a9c5
UZAIR SHAH <137003543+uzishah@users.noreply.github.com>
2023-10-21 21:26:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+
+
+//chapter no1
+
+//qo. no1
+//alert("asalamualikum")
+
+//qo. no2
+// alert("Error! Please enter valid password")
+
+//qo.3 
+
+// alert("welcome to JS land \n happy coding")
+
+// qno.4
+
+// alert("welcome to JS land \n happy coding")
+
+// alert("Error! Please enter valid password")
+
+
+
+
+//chapterno 2
+//1
+// var a = "username"
+// console.log(a)
+// //2
+// var myName = "syed uzair shah"
+// console.log(myName)
+// //3
+// var message = "Hello world"
+// alert(message)
+// //4
+
+// var name ="uzair"
+// var sec = "web & app"
+// var rollNo = 12
+
+// alert(name)
+// alert(sec)
+// alert(rollNo)
+
+// //5
+
+// var i = "shah \n shah \n syed \n uzair"
+// alert(i)
+
+
+// //6
+// var email = " my email is uzihah@gmail.com"
+// alert(email)
+
+
+
+// //qno.7
+//  var book = "A smarter way to learn JavaScript"
+//  alert(book)
+
+
+
+//qno.8
+
+// document.write("yeah ! i can wwrite html content through j.s")
+
+
+//qno. 9
+
+
+// var q = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(q)
+
+
+
+// chapter 3
+//qo1
+// var age = " im 20 years old"
+// alert (age);
+
+//qno2
+
+// var a = " you visit web page 15 times"
+// alert (a);
+
+//qno3
+
+// var birth = "my birth year is 19\n my data type is number"
+// document.write(birth)
+
+
+//qno4
+
+// var name = "john"
+// var product = "clothes"
+// var quntity = 20
+// var store = " al-shah"
+// document.write('welcome ' +name+ '. You have bought '+quntity+' of '+product+' from us at '+store)
+
+// chapter 4
+
+// var a, b, c;
+
+// var myName, $shah, _age, good;
+
+// var 1name, var, function,uzaie-shah;
+
+document.write("“Rules for naming JS variables”")
+document.write("avaScript, can include letters, numbers, underscores, and dollar signs. They must start with a letter, underscore (_), or dollar sign ($) and cannot be a reserved keyword or include spaces")
+
+
+
+
